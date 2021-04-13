ab85afca0b668847a7770de892b1cb951812be3e
Maris Graudins <maris.graudins@mg-software.eu>
2021-04-13 14:19:54 +0300

    custom ActiveQuery support.

--- src/Type/ActiveQueryDynamicMethodReturnTypeExtension.php
+++ src/Type/ActiveQueryDynamicMethodReturnTypeExtension.php
@@ -7,6 +7,7 @@ namespace Proget\PHPStan\Yii2\Type;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
+use PHPStan\Reflection\ParametersAcceptor;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\ArrayType;
@@ -20,54 +21,133 @@ use PHPStan\Type\StringType;
 use PHPStan\Type\ThisType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\UnionType;
+use yii\db\ActiveQuery;
 
 final class ActiveQueryDynamicMethodReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
     public function getClass(): string
     {
-        return 'yii\db\ActiveQuery';
+        return ActiveQuery::class;
     }
 
     public function isMethodSupported(MethodReflection $methodReflection): bool
     {
-        if (ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType() instanceof ThisType) {
+        // Needed for Array type forwarding
+        $returnType = ParametersAcceptorSelector::selectSingle($methodReflection->getVariants())->getReturnType();
+        if ($returnType instanceof ObjectType && $returnType->isInstanceOf(ActiveQuery::class)) {
             return true;
         }
 
-        return \in_array($methodReflection->getName(), ['one', 'all'], true);
+        return \in_array($methodReflection->getName(), ['asArray', 'one', 'all'], true);
     }
 
     public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
     {
-        $calledOnType = $scope->getType($methodCall->var);
-        if (!$calledOnType instanceof ActiveQueryObjectType) {
-            throw new ShouldNotHappenException(sprintf('Unexpected type %s during method call %s at line %d', \get_class($calledOnType), $methodReflection->getName(), $methodCall->getLine()));
-        }
-
         $methodName = $methodReflection->getName();
-        if ($methodName === 'asArray') {
-            $argType = isset($methodCall->args[0]) ? $scope->getType($methodCall->args[0]->value) : new ConstantBooleanType(true);
-            if (!$argType instanceof ConstantBooleanType) {
-                throw new ShouldNotHappenException(sprintf('Invalid argument provided to asArray method at line %d', $methodCall->getLine()));
-            }
 
-            return new ActiveQueryObjectType($calledOnType->getModelClass(), $argType->getValue());
+        if (\in_array($methodName, ['asArray'])) {
+            return $this->_asArray($methodReflection, $methodCall, $scope);
         }
 
-        if (!\in_array($methodName, ['one', 'all'], true)) {
-            return new ActiveQueryObjectType($calledOnType->getModelClass(), $calledOnType->isAsArray());
+        if (\in_array($methodName, ['one', 'all'])) {
+            return $this->_oneOrAll($methodReflection, $methodCall, $scope);
         }
 
+        // Forward type
+        return $scope->getType($methodCall->var);
+    }
+
+    private function _asArray(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ObjectType
+    {
+        $argType = isset($methodCall->args[0]) ? $scope->getType($methodCall->args[0]->value) : new ConstantBooleanType(true);
+        if (!$argType instanceof ConstantBooleanType) {
+            throw new ShouldNotHappenException(sprintf('Invalid argument provided to asArray method at line %d', $methodCall->getLine()));
+        }
+
+        $calledOnType = $scope->getType($methodCall->var);
+
+        switch (true) {
+            case $calledOnType instanceof ObjectType:
+                $className = $calledOnType->getClassName();
+                break;
+            case $calledOnType instanceof ThisType:
+                throw new ShouldNotHappenException('Please, use asArray outside Query class.');
+            default:
+                throw new ShouldNotHappenException('Unknown scope type: ' . get_class($calledOnType));
+        }
+
+        if ($argType->getValue()) {
+            return new ArrayActiveQueryObjectType($className);
+        }
+
+        return new ObjectType($className);
+    }
+
+    private function _oneOrAll(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+    {
+        $methodName = $methodReflection->getName();
+        $calledOnType = $scope->getType($methodCall->var);
+        $objectType = $this->_findObjectType($methodReflection->getVariants());
+        if (!$objectType) {
+            throw new ShouldNotHappenException(strtr('ObjectType return type not found for {c}::{m}()', [
+                '{c}' => $methodReflection->getDeclaringClass()->getName(),
+                '{m}' => $methodReflection->getName(),
+            ]));
+        }
+
+        $targetType = $calledOnType instanceof ArrayActiveQueryObjectType ? new ArrayType(new StringType(), new MixedType()) : $objectType;
+
+        // One
         if ($methodName === 'one') {
             return TypeCombinator::union(
                 new NullType(),
-                $calledOnType->isAsArray() ? new ArrayType(new StringType(), new MixedType()) : new ObjectType($calledOnType->getModelClass())
+                $targetType,
             );
         }
 
+        // All
         return new ArrayType(
             new IntegerType(),
-            $calledOnType->isAsArray() ? new ArrayType(new StringType(), new MixedType()) : new ObjectType($calledOnType->getModelClass())
+            $targetType,
         );
+    }
+
+    /**
+     * Recursive ObjectType finder
+     *
+     * @param mixed $input
+     * @return ObjectType|null
+     */
+    private function _findObjectType($input): ?ObjectType
+    {
+        // Walk through array
+        if (is_array($input)) {
+            foreach ($input as $value) {
+                if ($result = $this->_findObjectType($value)) {
+                    return $result;
+                }
+            }
+            return null;
+        }
+
+        if ($input instanceof ParametersAcceptor) {
+            return $this->_findObjectType($input->getReturnType());
+        }
+
+        if ($input instanceof UnionType) {
+            return $this->_findObjectType($input->getTypes());
+        }
+
+        if ($input instanceof ArrayType) {
+            return $this->_findObjectType($input->getItemType());
+        }
+
+        // Finally got it
+        if ($input instanceof ObjectType) {
+            return $input;
+        }
+
+        return null;
     }
 }
---
+++ src/Type/ArrayActiveQueryObjectType.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Proget\PHPStan\Yii2\Type;
+
+use PHPStan\Type\ObjectType;
+
+class ArrayActiveQueryObjectType extends ObjectType
+{
+}
